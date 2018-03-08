f1a7903e9999ecd524238064cfbbb41116daa278
1601156 <katiedear@btinternet.com>
2018-03-08 09:04:25 +0000

    Lab 12

---
+++ Lab12/main.js
@@ -0,0 +1,5 @@
+//set the map and initial coordinates
+var mymap = L.map('mapid').setView([0, 0], 1);
+
+var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ', maxZoom: 16});
+Esri_WorldGrayCanvas.addTo(mymap);
