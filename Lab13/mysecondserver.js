39eb1703caca75aa4ec436d202196676008c88e4
1601156 <katiedear@btinternet.com>
2018-03-12 15:25:41 +0000

    Lab 13

---
+++ Lab13/mysecondserver.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var currentdate = require('./mymodule');
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + currentdate.myDateTime());
+  res.end('Hello World!');
+}).listen(8080);
