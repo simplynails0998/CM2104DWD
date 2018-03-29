b0f460f14e59becece5a8e4d506de95431a612a5
1601156 <katiedear@btinternet.com>
2018-03-29 10:17:54 +0100

    Lab 15

--- Lab15/server.js
+++ Lab15/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
-app use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 var db;
 
